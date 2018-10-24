59f225dc436995c618e75070543cd9089819dfc7
SANJAY K SAI <shachsan@hotmail.com>
2018-10-24 03:06:47 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,25 +3,30 @@
 #with the correct path to this directory on your computer
 
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+# "Go Go GO" => '/Users/sanjaysai/Desktop/jukebox-cli/audio/Emerald-Park/01.mp3',
+# "LiberTeens" => '/Users/sanjaysai/Desktop/jukebox-cli/audio/Emerald-Park/02.mp3',
+# "Hamburg" =>  '/Users/sanjaysai/Desktop/jukebox-cli/audio/Emerald-Park/03.mp3',
+# "Guiding Light" => '/Users/sanjaysai/Desktop/jukebox-cli/audio/Emerald-Park/04.mp3',
+# "Wolf" => '/Users/sanjaysai/Desktop/jukebox-cli/audio/Emerald-Park/05.mp3',
+# "Blue" => '/Users/sanjaysai/Desktop/jukebox-cli/audio/Emerald-Park/06.mp3',
+# "Graduation Failed" => '/Users/sanjaysai/Desktop/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
 def help
-  #this method should be the same as in jukebox.rb
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  my_songs.keys.each_with_index{|song, i|puts "#{i+1}. #{song}"}
 end
 
 
@@ -33,13 +38,49 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name or number:"
+  song_choice = gets.chomp
+  choice = my_songs.find {|song_name, path|song_name == song_choice}
+  if choice
+    p choice
+    p choice.first
+    p choice.last
+    puts "Playing #{choice.first}"
+    open choice.last
+  else
+    puts "Invalid input, please try again"
+  end
+
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  puts "Welcome to your Jukebox!"
+  help
+
+  loop do
+    puts "Please enter a command:"
+    user_option = gets.chomp
+
+      case user_option
+        when "help"
+          help
+        when "list"
+          list(my_songs)
+        when "play"
+          play(my_songs)
+        when "exit"
+          exit_jukebox
+          break
+        else
+          "Please enter correct command"
+      end
+
+  end
+
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,99 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+# songs = [
+#   "Phoenix - 1901",
+#   "Tokyo Police Club - Wait Up",
+#   "Sufjan Stevens - Too Much",
+#   "The Naked and the Famous - Young Blood",
+#   "(Far From) Home - Tiga",
+#   "The Cults - Abducted",
+#   "Phoenix - Consolation Prizes",
+#   "Harry Chapin - Cats in the Cradle",
+#   "Amos Lee - Keep It Loose, Keep It Tight"
+# ]
 
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_choice = gets.chomp
+  choice = songs.find.with_index {|song, i|song == song_choice || song_choice == (i+1).to_s}
+  if choice
+    puts "Playing #{choice}"
+  else
+    puts "Invalid input, please try again"
+  end
+
+  ##Using case
+  # case song_choice
+  #     when "1" , "Phoenix - 1901"
+  #       puts "#{songs_list[0]}"
+  #
+  #     when "Tokyo Police Club - Wait Up" , "2"
+  #       puts "#{songs_list[1]}"
+  #
+  #     when "Sufjan Stevens - Too Much", "3"
+  #       puts "#{songs_list[2]}"
+  #
+  #     when "The Naked and the Famous - Young Blood", "4"
+  #       puts "#{songs_list[3]}"
+  #
+  #     when "(Far From) Home - Tiga", "5"
+  #       puts "#{songs_list[4]}"
+  #
+  #     when "The Cults - Abducted", "6"
+  #       puts "#{songs_list[5]}"
+  #
+  #     when "Phoenix - Consolation Prizes", "7"
+  #       puts "#{songs_list[6]}"
+  #
+  #     when "Harry Chapin - Cats in the Cradle", "8"
+  #       puts "#{songs_list[7]}"
+  #
+  #     when "Amos Lee - Keep It Loose, Keep It Tight", "9"
+  #       puts "#{songs_list[8]}"
+  #
+  #     else
+  #       puts "Invalid input, please try again"
+  # end
+
+end
+
+def list(songs)
+  songs.each_with_index {|song, i|puts "#{i+1}. #{song}"}
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts "Welcome to your Jukebox!"
+  help
+
+  loop do
+    puts "Please enter a command:"
+    user_option = gets.chomp
+
+      case user_option
+        when "help"
+          help
+        when "list"
+          list(songs)
+        when "play"
+          play(songs)
+        when "exit"
+          exit_jukebox
+          break
+        else
+          "Please enter correct command"
+      end
+
+  end
+
+end
